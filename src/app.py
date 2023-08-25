c43c7042df1611a0cb94b75c0de866cd8df50fa8
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2023-08-26 00:51:02 +0530

    initial commit

---
+++ src/app.py
@@ -0,0 +1,29 @@
+import uvicorn
+from dotenv import load_dotenv
+from fastapi import FastAPI, File, UploadFile
+from components import predict, read_imagefile
+import os
+
+load_dotenv(".env")
+app = FastAPI()
+
+
+@app.get("/")
+async def index():
+    return {"message": "Hello World"}
+
+
+@app.post("/predict/image")
+async def predict_api(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not extension:
+        return "Image must be jpg or png format!"
+    image = read_imagefile(await file.read())
+    prediction = predict(image)
+
+    return prediction
+
+
+if __name__ == "__main__":
+    print(os.getenv("PORT"))
+    uvicorn.run("app:app", port=int(os.getenv("PORT")), reload=True)
---
+++ src/components/__ init __.py
@@ -0,0 +1,1 @@
+from .components.serve_model import predict, read_imagefile
