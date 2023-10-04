7856e67eb2aee853f400f73a2535f193779f00ce
ASHUdev05 <amphoteric629@gmail.com>
2023-10-04 15:20:35 +0530

    Added the functionality as stated in #10 at the /compare api endpoint

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 import uvicorn
 from dotenv import load_dotenv
 from fastapi import FastAPI, File, UploadFile
-from .components.serve_model import predict, read_imagefile
+from .components.serve_model import predict, read_imagefile, compare_two_faces
 import os
 
 load_dotenv(".env")
@@ -22,6 +22,18 @@ async def predict_api(file: UploadFile = File(...)):
     prediction = predict(image)
 
     return prediction
+
+@app.post("/compare")
+async def compare_api(file1: UploadFile = File(...), file2: UploadFile = File(...)):
+    extension1 = file1.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    extension2 = file2.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not extension1 or not extension2:
+        return "Image must be jpg or png format!"
+    image1 = read_imagefile(await file1.read())
+    image2 = read_imagefile(await file2.read())
+    result = compare_two_faces(image1, image2)
+
+    return result
 
 
 if __name__ == "__main__":
